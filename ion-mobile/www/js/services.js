ece1fafbd9db5d0fc48cd1ca53a3a254fff68aae
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-02-11 22:50:10 +0200

    added local storage to favorites

--- ion-mobile/www/js/services.js
+++ ion-mobile/www/js/services.js
@@ -30,9 +30,9 @@ angular.module('conFusion.services', ['ngResource'])
             return $resource(baseURL+"feedback/:id");
 
         }])
-        .factory('favoriteFactory', ['$resource', 'baseURL', function($resource,baseURL){
+        .factory('favoriteFactory', ['$resource', 'baseURL', '$localStorage' ,function($resource,baseURL, $localStorage){
             var favFac = {};
-            var favorites = [];
+            var favorites = $localStorage.getObject('favorites', '[]');
 
             favFac.addToFavorites = function (index) {
               for (var i = 0; i < favorites.length; i++) {
@@ -40,6 +40,7 @@ angular.module('conFusion.services', ['ngResource'])
                   return;
               }
               favorites.push({id: index});
+              $localStorage.storeObject('favorites', favorites);
             };
 
            favFac.deleteFromFavorites = function(index) {
@@ -48,10 +49,11 @@ angular.module('conFusion.services', ['ngResource'])
                  favorites.splice(i,1);
                }
              }
+             $localStorage.storeObject('favorites', favorites);
            };
 
             favFac.getFavorites = function () {
-              return favorites;
+              return $localStorage.getObject('favorites', '[]');
             };
 
             return favFac;
